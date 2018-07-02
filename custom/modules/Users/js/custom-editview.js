2c046cbd2b8926b8228ecb67959adbe0c008c0f6
DESKTOP-LV5G7FI\steven <stevenkyamko@outlook.com>
2018-07-03 02:40:51 +0800

    CHANGE DEFAULT LAYOUT TO "DAY"

---
+++ custom/modules/Users/js/custom-editview.js
@@ -0,0 +1,9 @@
+$(document).ready(function(e){
+	/**
+     * This will set the default value of Style to Day.
+     * @author Steven Kyamko
+     * @date_created 7/3/2018
+	**/
+	$('select[name="subtheme"]').val('Day');
+	/** End of customization **/
+});

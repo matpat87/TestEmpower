53a0f745ec272ac5274d458a39f0f8f07f128c82
DESKTOP-LV5G7FI\steven <stevenkyamko@outlook.com>
2018-07-07 17:09:48 +0800

    Revert "removed Ext"
    This reverts commit ea11dca82ba2472cfea58faae7301ad586b7f7ff.

---
+++ custom/modules/EHS_EHS/Ext/Language/en_us.lang.ext.php
@@ -0,0 +1,19 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
+$mod_strings['LBL_EHS_EHS_DOCUMENTS_1_FROM_DOCUMENTS_TITLE'] = 'Documents';
+$mod_strings['LBL_EHS_EHS_CALLS_1_FROM_CALLS_TITLE'] = 'Calls';
+
+
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
+$mod_strings['LBL_EHS_EHS_DOCUMENTS_1_FROM_DOCUMENTS_TITLE'] = 'Documents';
+
+
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
+$mod_strings['LBL_EHS_EHS_DOCUMENTS_1_FROM_DOCUMENTS_TITLE'] = 'Documents';
+$mod_strings['LBL_EHS_EHS_CALLS_1_FROM_CALLS_TITLE'] = 'Calls';
+$mod_strings['LBL_EHS_EHS_MEETINGS_1_FROM_MEETINGS_TITLE'] = 'Meetings';
+
+?>
---
+++ custom/modules/EHS_EHS/Ext/Layoutdefs/layoutdefs.ext.php
@@ -0,0 +1,108 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+ // created: 2018-07-03 16:48:14
+$layout_defs["EHS_EHS"]["subpanel_setup"]['ehs_ehs_calls_1'] = array (
+  'order' => 5,
+  'module' => 'Calls',
+  'subpanel_name' => 'default',
+  'sort_order' => 'asc',
+  'sort_by' => 'id',
+  'title_key' => 'LBL_EHS_EHS_CALLS_1_FROM_CALLS_TITLE',
+  'get_subpanel_data' => 'ehs_ehs_calls_1',
+  'top_buttons' => 
+  array (
+    0 => 
+    array (
+      'widget_class' => 'SubPanelTopButtonQuickCreate',
+    ),
+    1 => 
+    array (
+      'widget_class' => 'SubPanelTopSelectButton',
+      'mode' => 'MultiSelect',
+    ),
+  ),
+);
+
+
+ // created: 2018-07-03 16:43:44
+$layout_defs["EHS_EHS"]["subpanel_setup"]['ehs_ehs_documents_1'] = array (
+  'order' => 15,
+  'module' => 'Documents',
+  'subpanel_name' => 'default',
+  'sort_order' => 'asc',
+  'sort_by' => 'id',
+  'title_key' => 'LBL_EHS_EHS_DOCUMENTS_1_FROM_DOCUMENTS_TITLE',
+  'get_subpanel_data' => 'ehs_ehs_documents_1',
+  'top_buttons' => 
+  array (
+    0 => 
+    array (
+      'widget_class' => 'SubPanelTopButtonQuickCreate',
+    ),
+    1 => 
+    array (
+      'widget_class' => 'SubPanelTopSelectButton',
+      'mode' => 'MultiSelect',
+    ),
+  ),
+);
+
+
+ // created: 2018-07-03 16:51:31
+$layout_defs["EHS_EHS"]["subpanel_setup"]['ehs_ehs_meetings_1'] = array (
+  'order' => 10,
+  'module' => 'Meetings',
+  'subpanel_name' => 'default',
+  'sort_order' => 'asc',
+  'sort_by' => 'id',
+  'title_key' => 'LBL_EHS_EHS_MEETINGS_1_FROM_MEETINGS_TITLE',
+  'get_subpanel_data' => 'ehs_ehs_meetings_1',
+  'top_buttons' => 
+  array (
+    0 => 
+    array (
+      'widget_class' => 'SubPanelTopButtonQuickCreate',
+    ),
+    1 => 
+    array (
+      'widget_class' => 'SubPanelTopSelectButton',
+      'mode' => 'MultiSelect',
+    ),
+  ),
+);
+
+
+ // created: 2018-07-03 16:48:14
+$layout_defs["EHS_EHS"]["subpanel_setup"]['securitygroups_ehs_ehs'] = array (
+  'order' => 20,
+  'module' => 'SecurityGroups',
+  'subpanel_name' => 'default',
+  'sort_order' => 'asc',
+  'sort_by' => 'id',
+  'title_key' => 'LBL_SECURITYGROUPS_EHS_EHS_TITLE',
+  'get_subpanel_data' => 'SecurityGroups',
+  'top_buttons' => 
+  array (
+    0 => 
+    array (
+      'widget_class' => 'SubPanelTopButtonQuickCreate',
+    ),
+    1 => 
+    array (
+      'widget_class' => 'SubPanelTopSelectButton',
+      'mode' => 'MultiSelect',
+    ),
+  ),
+);
+
+
+//auto-generated file DO NOT EDIT
+$layout_defs['EHS_EHS']['subpanel_setup']['ehs_ehs_meetings_1']['override_subpanel_name'] = 'EHS_EHS_subpanel_ehs_ehs_meetings_1';
+
+
+//auto-generated file DO NOT EDIT
+$layout_defs['EHS_EHS']['subpanel_setup']['securitygroups_ehs_ehs']['override_subpanel_name'] = 'EHS_EHS_subpanel_securitygroups_ehs_ehs';
+
+?>

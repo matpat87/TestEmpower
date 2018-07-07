ccb5a91a3fa1fcc369705b6fd749f7b9e79d9c3f
DESKTOP-LV5G7FI\steven <stevenkyamko@outlook.com>
2018-07-07 15:52:31 +0800

    Revert "Revert "added subpanels for Regulatory module""
    This reverts commit 0ed2dfbb84d0484fefd88db82a77e973f4db9ded.

--- custom/Extension/modules/RE_Regulatory/Ext/Layoutdefs/securitygroups_RE_Regulatory.php
+++ custom/Extension/modules/RE_Regulatory/Ext/Layoutdefs/securitygroups_RE_Regulatory.php
@@ -1,13 +1,13 @@
 <?php
- // created: 2018-06-23 09:09:53
-$layout_defs["RE_Regulatory"]["subpanel_setup"]['re_regulatory_documents_1'] = array (
-  'order' => 15,
-  'module' => 'Documents',
+ // created: 2018-07-03 16:48:14
+$layout_defs["RE_Regulatory"]["subpanel_setup"]['securitygroups_re_regulatory'] = array (
+  'order' => 20,
+  'module' => 'SecurityGroups',
   'subpanel_name' => 'default',
   'sort_order' => 'asc',
   'sort_by' => 'id',
-  'title_key' => 'LBL_RE_REGULATORY_DOCUMENTS_1_FROM_DOCUMENTS_TITLE',
-  'get_subpanel_data' => 're_regulatory_documents_1',
+  'title_key' => 'LBL_SECURITYGROUPS_RE_REGULATORY_TITLE',
+  'get_subpanel_data' => 'SecurityGroups',
   'top_buttons' => 
   array (
     0 => 

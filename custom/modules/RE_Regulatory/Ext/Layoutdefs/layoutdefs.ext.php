25b88ade2c51567d8db39ac5b4746e71dbd5d1fd
DESKTOP-LV5G7FI\steven <stevenkyamko@outlook.com>
2018-07-07 15:21:53 +0800

    added subpanels for Regulatory module

--- custom/modules/RE_Regulatory/Ext/Layoutdefs/layoutdefs.ext.php
+++ custom/modules/RE_Regulatory/Ext/Layoutdefs/layoutdefs.ext.php
@@ -4,7 +4,7 @@
 
  // created: 2018-06-23 09:08:23
 $layout_defs["RE_Regulatory"]["subpanel_setup"]['re_regulatory_calls_1'] = array (
-  'order' => 100,
+  'order' => 10,
   'module' => 'Calls',
   'subpanel_name' => 'default',
   'sort_order' => 'asc',
@@ -28,7 +28,7 @@ $layout_defs["RE_Regulatory"]["subpanel_setup"]['re_regulatory_calls_1'] = array
 
  // created: 2018-06-23 09:09:53
 $layout_defs["RE_Regulatory"]["subpanel_setup"]['re_regulatory_documents_1'] = array (
-  'order' => 100,
+  'order' => 15,
   'module' => 'Documents',
   'subpanel_name' => 'default',
   'sort_order' => 'asc',
@@ -52,13 +52,37 @@ $layout_defs["RE_Regulatory"]["subpanel_setup"]['re_regulatory_documents_1'] = a
 
  // created: 2018-06-23 09:03:50
 $layout_defs["RE_Regulatory"]["subpanel_setup"]['re_regulatory_tasks_1'] = array (
-  'order' => 100,
+  'order' => 5,
   'module' => 'Tasks',
   'subpanel_name' => 'default',
   'sort_order' => 'asc',
   'sort_by' => 'id',
   'title_key' => 'LBL_RE_REGULATORY_TASKS_1_FROM_TASKS_TITLE',
   'get_subpanel_data' => 're_regulatory_tasks_1',
+  'top_buttons' => 
+  array (
+    0 => 
+    array (
+      'widget_class' => 'SubPanelTopButtonQuickCreate',
+    ),
+    1 => 
+    array (
+      'widget_class' => 'SubPanelTopSelectButton',
+      'mode' => 'MultiSelect',
+    ),
+  ),
+);
+
+
+ // created: 2018-07-03 16:48:14
+$layout_defs["RE_Regulatory"]["subpanel_setup"]['securitygroups_re_regulatory'] = array (
+  'order' => 20,
+  'module' => 'SecurityGroups',
+  'subpanel_name' => 'default',
+  'sort_order' => 'asc',
+  'sort_by' => 'id',
+  'title_key' => 'LBL_SECURITYGROUPS_RE_REGULATORY_TITLE',
+  'get_subpanel_data' => 'SecurityGroups',
   'top_buttons' => 
   array (
     0 => 

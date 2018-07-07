bafcd541cd21a3d1b4c8372becd9b138df506996
root <root@localhost.localdomain>
2018-07-07 02:56:50 -0400

    fixed untracked files

--- custom/modules/EHS_EHS/Ext/Vardefs/vardefs.ext.php
+++ custom/modules/EHS_EHS/Ext/Vardefs/vardefs.ext.php
@@ -38,15 +38,15 @@ $dictionary["EHS_EHS"]["fields"]["ehs_ehs_meetings_1"] = array (
 );
 
 
- // created: 2018-06-20 18:11:17
-$dictionary['EHS_EHS']['fields']['actionitem_assigned_user_c']['inline_edit']='1';
-$dictionary['EHS_EHS']['fields']['actionitem_assigned_user_c']['labelValue']='Action Item Assigned to';
-
- 
-
  // created: 2018-06-20 16:11:52
 $dictionary['EHS_EHS']['fields']['action_items_c']['inline_edit']='1';
 $dictionary['EHS_EHS']['fields']['action_items_c']['labelValue']='Action Item';
+
+ 
+
+ // created: 2018-06-20 18:11:17
+$dictionary['EHS_EHS']['fields']['actionitem_assigned_user_c']['inline_edit']='1';
+$dictionary['EHS_EHS']['fields']['actionitem_assigned_user_c']['labelValue']='Action Item Assigned to';
 
  
 

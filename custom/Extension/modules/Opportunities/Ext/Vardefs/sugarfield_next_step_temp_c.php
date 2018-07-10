b2f30e7157f284b21b0fba3bd5bee72c109fb69d
DESKTOP-LV5G7FI\steven <stevenkyamko@outlook.com>
2018-07-11 01:19:05 +0800

    Opportunity Changes updates

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_next_step_temp_c.php
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_next_step_temp_c.php
@@ -2,5 +2,5 @@
  // created: 2018-07-09 18:31:12
 $dictionary['Opportunity']['fields']['next_step_temp_c']['inline_edit']='1';
 $dictionary['Opportunity']['fields']['next_step_temp_c']['labelValue']='Next Step';
-
+$dictionary['Opportunity']['fields']['next_step_temp_c']['required'] = false;
  ?>

1ca32ffd47fb60cbe34c54774a1d3c9e26a63c64
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2018-07-11 00:04:42 +0800

    Markets Module - Fixed length and precision points of gross margin, growth rate, and market penetration

---
+++ custom/Extension/modules/MKT_Markets/Ext/Vardefs/sugarfield_gross_margin.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2018-07-10 17:58:15
+$dictionary['MKT_Markets']['fields']['gross_margin']['len']='4';
+$dictionary['MKT_Markets']['fields']['gross_margin']['precision']='1';
+
+ ?>
--- custom/modules/MKT_Markets/language/en_us.lang.php
+++ custom/modules/MKT_Markets/language/en_us.lang.php
@@ -1,8 +1,11 @@
 <?php
-// created: 2018-07-10 17:15:19
+// created: 2018-07-10 17:58:25
 $mod_strings = array (
   'LBL_REGION' => 'Region',
   'LBL_MARKETS_SUBPANEL_TITLE' => 'Markets',
   'LBL_CAMPAINGS_SUBPANEL_TITLE' => 'Markets',
   'LBL_SECURITYGROUPS_MKT_MARKETS_TITLE' => 'Security Groups',
+  'LBL_MARKET_PENETRATION' => 'Market Proposition %',
+  'LBL_GROSS_MARGIN' => 'Gross Margin %',
+  'LBL_GROWTH_RATE' => 'Growth Rate %',
 );

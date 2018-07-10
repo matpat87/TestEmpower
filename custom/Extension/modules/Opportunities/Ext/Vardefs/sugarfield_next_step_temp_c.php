37986a74f19c444f3314f6f161bafa3e5bdb06fe
DESKTOP-LV5G7FI\steven <stevenkyamko@outlook.com>
2018-07-11 01:30:55 +0800

    Opportunity Screen Changes - removed required of next_temp_c

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_next_step_temp_c.php
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_next_step_temp_c.php
@@ -1,6 +1,6 @@
 <?php
- // created: 2018-07-09 18:31:12
+ // created: 2018-07-10 17:26:45
 $dictionary['Opportunity']['fields']['next_step_temp_c']['inline_edit']='1';
 $dictionary['Opportunity']['fields']['next_step_temp_c']['labelValue']='Next Step';
-$dictionary['Opportunity']['fields']['next_step_temp_c']['required'] = false;
+
  ?>

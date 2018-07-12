458bb690d1c34b061e7697cbdbfaaa199edd82f0
DESKTOP-LV5G7FI\steven <stevenkyamko@outlook.com>
2018-07-13 03:43:05 +0800

    Reports Module update

--- custom/application/Ext/Include/modules.ext.php
+++ custom/application/Ext/Include/modules.ext.php
@@ -3,9 +3,30 @@
 
  
  //WARNING: The contents of this file are auto-generated
+$beanList['COMP_Competition'] = 'COMP_Competition';
+$beanFiles['COMP_Competition'] = 'modules/COMP_Competition/COMP_Competition.php';
+$moduleList[] = 'COMP_Competition';
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+$beanList['RPT_CRMReports'] = 'RPT_CRMReports';
+$beanFiles['RPT_CRMReports'] = 'modules/RPT_CRMReports/RPT_CRMReports.php';
+$moduleList[] = 'RPT_CRMReports';
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
 $beanList['EHS_EHS'] = 'EHS_EHS';
 $beanFiles['EHS_EHS'] = 'modules/EHS_EHS/EHS_EHS.php';
 $moduleList[] = 'EHS_EHS';
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+$beanList['MKT_Markets'] = 'MKT_Markets';
+$beanFiles['MKT_Markets'] = 'modules/MKT_Markets/MKT_Markets.php';
+$moduleList[] = 'MKT_Markets';
 
 
  
--- custom/application/Ext/TableDictionary/tabledictionary.ext.php
+++ custom/application/Ext/TableDictionary/tabledictionary.ext.php
@@ -3,6 +3,15 @@
 
  
  //WARNING: The contents of this file are auto-generated
+include('custom/metadata/accounts_comp_competition_1MetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/campaigns_marketsMetaData.php');
+
+ 
+ //WARNING: The contents of this file are auto-generated
 include('custom/metadata/ehs_ehs_calls_1MetaData.php');
 
 
@@ -14,6 +23,31 @@ include('custom/metadata/ehs_ehs_documents_1MetaData.php');
  
  //WARNING: The contents of this file are auto-generated
 include('custom/metadata/ehs_ehs_meetings_1MetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/mkt_markets_accounts_1MetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/mkt_markets_documents_1MetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/mkt_markets_leads_1MetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/mkt_markets_opportunities_1MetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/mkt_markets_prospectlists_1MetaData.php');
 
 
  

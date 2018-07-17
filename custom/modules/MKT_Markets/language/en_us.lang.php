d192cc42e902d9aa4567292db9dd238507ddc2e6
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2018-07-17 22:44:19 +0800

    Markets Module - Renamed "Market Proposition %" to "Market Penetration %"

--- custom/modules/MKT_Markets/language/en_us.lang.php
+++ custom/modules/MKT_Markets/language/en_us.lang.php
@@ -1,11 +1,11 @@
 <?php
-// created: 2018-07-10 17:58:25
+// created: 2018-07-17 16:41:06
 $mod_strings = array (
   'LBL_REGION' => 'Region',
   'LBL_MARKETS_SUBPANEL_TITLE' => 'Markets',
   'LBL_CAMPAINGS_SUBPANEL_TITLE' => 'Markets',
   'LBL_SECURITYGROUPS_MKT_MARKETS_TITLE' => 'Security Groups',
-  'LBL_MARKET_PENETRATION' => 'Market Proposition %',
+  'LBL_MARKET_PENETRATION' => 'Market Penetration %',
   'LBL_GROSS_MARGIN' => 'Gross Margin %',
   'LBL_GROWTH_RATE' => 'Growth Rate %',
 );

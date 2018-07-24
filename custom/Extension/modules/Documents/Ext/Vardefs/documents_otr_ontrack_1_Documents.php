3c53e35a66baa44725433b07a2166a99a135c744
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2018-07-24 23:01:57 +0800

    On Track & Documents Module - Added Many-to-many relationship between Documents and On Track Modules

---
+++ custom/Extension/modules/Documents/Ext/Vardefs/documents_otr_ontrack_1_Documents.php
@@ -0,0 +1,11 @@
+<?php
+// created: 2018-07-24 16:54:06
+$dictionary["Document"]["fields"]["documents_otr_ontrack_1"] = array (
+  'name' => 'documents_otr_ontrack_1',
+  'type' => 'link',
+  'relationship' => 'documents_otr_ontrack_1',
+  'source' => 'non-db',
+  'module' => 'OTR_OnTrack',
+  'bean_name' => 'OTR_OnTrack',
+  'vname' => 'LBL_DOCUMENTS_OTR_ONTRACK_1_FROM_OTR_ONTRACK_TITLE',
+);

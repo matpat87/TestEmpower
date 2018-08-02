395775b14aa37201173b82e9968d73568f58b2f2
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2018-08-02 23:16:07 +0800

    Contacts Module - In editviewdefs, changed type 'address' to 'DropdownCountryAddress' for primary_address_street and alt_address_street - Created custom vardefs to change field types to enum and retrieve dropdown options from 'countries_list' and 'states_list'

---
+++ custom/Extension/modules/Contacts/Ext/Vardefs/custom_primary_address_country.php
@@ -0,0 +1,6 @@
+<?php
+$dictionary['Contact']['fields']['primary_address_country']['comments']='Country for primary address';
+$dictionary['Contact']['fields']['primary_address_country']['group']='billing_address';
+$dictionary['Contact']['fields']['primary_address_country']['options']='countries_list';
+$dictionary['Contact']['fields']['primary_address_country']['type']='enum';
+$dictionary['Contact']['fields']['primary_address_country']['inline_edit']='0';

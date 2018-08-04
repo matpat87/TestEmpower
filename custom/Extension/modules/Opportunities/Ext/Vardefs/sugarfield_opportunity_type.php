d547bf7408703e04d0ee78a6b2a59744ed975b7a
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2018-08-04 16:41:33 +0800

    OnTrack Module - Removed changes made for ticket 48
    Opportunities Module
    - Removed changes made for ticket 34

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_opportunity_type.php
+++
@@ -1,9 +0,0 @@
-<?php
- // created: 2018-07-19 15:36:14
-$dictionary['Opportunity']['fields']['opportunity_type']['len']=100;
-$dictionary['Opportunity']['fields']['opportunity_type']['required']=true;
-$dictionary['Opportunity']['fields']['opportunity_type']['inline_edit']=true;
-$dictionary['Opportunity']['fields']['opportunity_type']['comments']='Type of opportunity (ex: Existing, New)';
-$dictionary['Opportunity']['fields']['opportunity_type']['merge_filter']='disabled';
-
- ?>

fe3717c3b7c4f23e06c14a24b85ffaf238457af3
DESKTOP-LV5G7FI\steven <stevenkyamko@outlook.com>
2018-08-04 17:39:09 +0800

    OnTrack 5 - remove codes

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_next_step.php
+++
@@ -1,9 +0,0 @@
-<?php
- // created: 2018-06-18 17:16:02
-$dictionary['Opportunity']['fields']['next_step']['comments'] ='Next Step';
-$dictionary['Opportunity']['fields']['next_step']['type'] ='html';
-$dictionary['Opportunity']['fields']['next_step']['len'] ='50000';
-$dictionary['Opportunity']['fields']['next_step']['inline_edit'] = '0';
-
-
- ?>

fab14c6f1674bacd285d56aa2365ffdc023ef4a1
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2018-08-13 23:10:46 +0800

    - Added Import feature for custom Markets Module

--- modules/MKT_Markets/Menu.php
+++ modules/MKT_Markets/Menu.php
@@ -50,3 +50,6 @@ if(ACLController::checkAccess('MKT_Markets', 'edit', true)){
 if(ACLController::checkAccess('MKT_Markets', 'list', true)){
     $module_menu[]=array('index.php?module=MKT_Markets&action=index&return_module=MKT_Markets&return_action=DetailView', $mod_strings['LNK_LIST'],'View', 'MKT_Markets');
 }
+if(ACLController::checkAccess('MKT_Markets', 'import', true)){
+    $module_menu[]=array('index.php?module=Import&action=Step1&import_module=MKT_Markets&return_module=MKT_Markets&return_action=index', $app_strings['LBL_IMPORT'], 'Import', 'MKT_Markets');
+}

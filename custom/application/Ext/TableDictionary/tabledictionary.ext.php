d161e1952683a35d2e3a931cb9be26eac2b56b4c
Steven O. Kyamko <stevenkyamko@gmail.com>
2018-08-14 10:51:23 +0800

    remove Opportunities and Accounts duplicate relationship

--- custom/application/Ext/TableDictionary/tabledictionary.ext.php
+++ custom/application/Ext/TableDictionary/tabledictionary.ext.php
@@ -57,11 +57,6 @@ include('custom/metadata/mkt_markets_prospectlists_1MetaData.php');
 
  
  //WARNING: The contents of this file are auto-generated
-include('custom/metadata/opportunities_accounts_1MetaData.php');
-
-
- 
- //WARNING: The contents of this file are auto-generated
 include('custom/metadata/opportunities_project_1MetaData.php');
 
 

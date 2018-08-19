b26d5587dbd0d10f7e1bb6aeaa327a52ba26ae16
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2018-08-19 23:39:37 +0800

    Security Groups Module - Manually added Accounts, Contacts, Customer Issues, and Opportunities since by default Security Group does not display the subpanel - Modified core file (data/Relationships/M2MRelationship.php) to fix issue where newly added subpanels "Select" action is not working

--- custom/Extension/modules/SecurityGroups/Ext/Layoutdefs/securitygroups_opportunities_SecurityGroups.php
+++ custom/Extension/modules/SecurityGroups/Ext/Layoutdefs/securitygroups_opportunities_SecurityGroups.php
@@ -1,13 +1,13 @@
 <?php
- // created: 2018-07-04 17:18:26
-$layout_defs["SecurityGroups"]["subpanel_setup"]['securitygroups_comp_competition'] = array (
+ // created: 2018-08-19 15:53:46
+$layout_defs["SecurityGroups"]["subpanel_setup"]['securitygroups_opportunities'] = array (
   'order' => 100,
-  'module' => 'COMP_Competition',
+  'module' => 'Opportunities',
   'subpanel_name' => 'default',
   'sort_order' => 'asc',
   'sort_by' => 'id',
-  'title_key' => 'LBL_SECURITYGROUPS_COMP_COMPETITION_FROM_COMP_COMPETITION_TITLE',
-  'get_subpanel_data' => 'SecurityGroups',
+  'title_key' => 'LBL_SECURITYGROUPS_OPPORTUNITIES_FROM_OPPORTUNITIES_TITLE',
+  'get_subpanel_data' => 'securitygroups_opportunities',
   'top_buttons' => 
   array (
     0 => 
---
+++ custom/Extension/modules/SecurityGroups/Ext/Vardefs/securitygroups_contacts_SecurityGroups.php
@@ -0,0 +1,11 @@
+<?php
+// created: 2018-08-19 15:53:46
+$dictionary["SecurityGroup"]["fields"]["securitygroups_contacts"] = array (
+  'name' => 'securitygroups_contacts',
+  'type' => 'link',
+  'relationship' => 'securitygroups_contacts',
+  'source' => 'non-db',
+  'module' => 'Contacts',
+  'bean_name' => 'Contact',
+  'vname' => 'LBL_SECURITYGROUPS_CONTACTS_FROM_CONTACTS_TITLE',
+);

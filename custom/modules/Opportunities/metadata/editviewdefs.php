31441738636aae2984fed1ccdefc0e09b0984381
Steven O. Kyamko <stevenkyamko@gmail.com>
2018-09-09 12:28:36 +0800

    added codes for Opportunities

---
+++ custom/modules/Opportunities/metadata/editviewdefs.php
@@ -0,0 +1,114 @@
+<?php
+$viewdefs ['Opportunities'] = 
+array (
+  'EditView' => 
+  array (
+    'templateMeta' => 
+    array (
+      'maxColumns' => '2',
+      'widths' => 
+      array (
+        0 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+        1 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+      ),
+      'javascript' => '{$PROBABILITY_SCRIPT}',
+      'useTabs' => true,
+      'tabDefs' => 
+      array (
+        'DEFAULT' => 
+        array (
+          'newTab' => true,
+          'panelDefault' => 'expanded',
+        ),
+        'LBL_PANEL_ASSIGNMENT' => 
+        array (
+          'newTab' => false,
+          'panelDefault' => 'expanded',
+        ),
+      ),
+    ),
+    'panels' => 
+    array (
+      'default' => 
+      array (
+        0 => 
+        array (
+          0 => 
+          array (
+            'name' => 'name',
+          ),
+          1 => 'account_name',
+        ),
+        1 => 
+        array (
+          0 => 
+          array (
+            'name' => 'currency_id',
+            'label' => 'LBL_CURRENCY',
+          ),
+          1 => 
+          array (
+            'name' => 'date_closed',
+          ),
+        ),
+        2 => 
+        array (
+          0 => 
+          array (
+            'name' => 'amount',
+            'customCode' => '<input type="text" name="{$fields.amount.name}" id="{$fields.amount.name}" value="{$fields.amount.value}" maxlength="{$fields.amount.len}" class="custom-currency" />',
+          ),
+          1 => 'opportunity_type',
+        ),
+        3 => 
+        array (
+          0 => 'sales_stage',
+          1 => 'lead_source',
+        ),
+        4 => 
+        array (
+          0 => 'probability',
+          1 => '',
+        ),
+        5 => 
+        array (
+          0 => 'next_step_temp_c',
+        ),
+        6 => 
+        array (
+          0 => 'description',
+          1 => 
+          array (
+            'name' => 'mkt_markets_opportunities_1_name',
+          ),
+        ),
+      ),
+      'LBL_PANEL_ASSIGNMENT' => 
+      array (
+        0 => 
+        array (
+          0 => 'assigned_user_name',
+        ),
+      ),
+    ),
+  ),
+);
+
+
+
+$viewdefs['Opportunities']['EditView']['templateMeta']['includes'] =
+    array (
+        array (
+        'file' => 'custom/modules/Opportunities/js/custom-edit.js',
+        ),
+    );
+
+?>

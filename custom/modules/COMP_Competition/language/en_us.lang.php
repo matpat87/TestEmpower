139e09c05f412d99523757ec1f8ed4d783c7baab
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2018-09-13 22:11:23 +0800

    Competition Module - Added new Mfg Type dropdown field

--- custom/modules/COMP_Competition/language/en_us.lang.php
+++ custom/modules/COMP_Competition/language/en_us.lang.php
@@ -1,5 +1,6 @@
 <?php
-// created: 2018-07-09 17:08:31
+// created: 2018-09-13 16:05:19
 $mod_strings = array (
   'LBL_SECURITYGROUPS_COMP_COMPETITION_TITLE' => 'Security Groups',
+  'LBL_MANUFACTURING_TYPE' => 'Mfg Type',
 );

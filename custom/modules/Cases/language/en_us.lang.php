1beb5261a10ea20fce168b217834aabe5c655fb0
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2018-10-31 23:30:57 +0800

    Customer Issues (Cases) Module - Added new dropdown fields with their corresponding dropdown values based on attached document in OnTrack Issue #42 - Status and Priority are existing fields, dropdown values are updated based on the attached document

--- custom/modules/Cases/language/en_us.lang.php
+++ custom/modules/Cases/language/en_us.lang.php
@@ -1,5 +1,5 @@
 <?php
-// created: 2018-08-18 18:45:17
+// created: 2018-10-31 15:58:24
 $mod_strings = array (
   'LNK_NEW_CASE' => 'Create Customer Issue',
   'LNK_CASE_LIST' => 'View Customer Issues',
@@ -8,4 +8,11 @@ $mod_strings = array (
   'LBL_SEARCH_FORM_TITLE' => 'Customer Issue Search',
   'LBL_LIST_MY_CASES' => 'My Open Customer Issues',
   'LBL_MODULE_NAME' => 'Customer Issues',
+  'LBL_CUSTOMER_PROCESS' => 'Customer Process',
+  'LBL_CUSTOMER_MATERIAL_DISPO' => 'Customer Material Dispo',
+  'LBL_INTERNAL_MATERIAL_DISPO' => 'Internal Material Dispo',
+  'LBL_SITE' => 'Site',
+  'LBL_CATEGORY' => 'Category',
+  'LBL_PRIORITY' => 'Priority:',
+  'LBL_STATUS' => 'Status:',
 );

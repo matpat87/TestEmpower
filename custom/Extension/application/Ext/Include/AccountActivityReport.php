4615db87b78fa4a9169c88ce8c3a6b66bd858cc9
stevenkyamko <stevenkyamko@gmail.com>
2018-11-13 12:13:44 +0800

    Module builder for Account Activity

---
+++ custom/Extension/application/Ext/Include/AccountActivityReport.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['AAR_AccountActivityReport'] = 'AAR_AccountActivityReport';
+$beanFiles['AAR_AccountActivityReport'] = 'modules/AAR_AccountActivityReport/AAR_AccountActivityReport.php';
+$moduleList[] = 'AAR_AccountActivityReport';
+
+?>

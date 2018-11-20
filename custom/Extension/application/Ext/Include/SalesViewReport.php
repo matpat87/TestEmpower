2795f6110230471da3806bfcce23d657b3a37965
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2018-11-21 00:15:09 +0800

    Sales View Report - Added SVR_SalesViewReport module (via Studio) - Modified Menu to have similar display as other reports (show "View CRM Reports" link only) - Hid unnecessary layout in List View page - Added codes to append logged user ID when accessing Sales View Report page - Added iFrame for external Sales View Report - Append URL in iFrame using the provided URL(retrieved from ticket 28 description) + logged user's user name to retrieve report data

---
+++ custom/Extension/application/Ext/Include/SalesViewReport.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['SVR_SalesViewReport'] = 'SVR_SalesViewReport';
+$beanFiles['SVR_SalesViewReport'] = 'modules/SVR_SalesViewReport/SVR_SalesViewReport.php';
+$moduleList[] = 'SVR_SalesViewReport';
+
+?>

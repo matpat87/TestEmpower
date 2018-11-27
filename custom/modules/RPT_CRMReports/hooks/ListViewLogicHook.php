6f87732437c0938943eae849be1ee062bef326c6
stevenkyamko <stevenkyamko@gmail.com>
2018-11-27 10:37:09 +0800

    Merge 125 to master

--- custom/modules/RPT_CRMReports/hooks/ListViewLogicHook.php
+++ custom/modules/RPT_CRMReports/hooks/ListViewLogicHook.php
@@ -5,6 +5,7 @@ class ListViewLogicHook {
     public function getURL(&$bean, $event, $arguments) {
         global $sugar_config;
 
+        $site_url = $sugar_config['site_url'];
         $record_bean = BeanFactory::getBean('RPT_CRMReports');
         $record_bean->retrieve($bean->id);
         $array_link = 'module=' . $record_bean->module_url_c;
@@ -26,7 +27,13 @@ class ListViewLogicHook {
 	        $array_link = http_build_query($request);
     	}
 
-        $url = $sugar_config['site_url'] . '/index.php?action=index&' . $array_link;
+    	//change URL if chromacolors
+    	if(strpos($_SERVER['SERVER_NAME'], 'chromacolors') !== false)
+    	{
+    		$site_url = string_replace_all("carolinacolor", "chromacolors", $site_url);
+    	}
+
+        $url = $site_url . '/index.php?action=index&' . $array_link;
 
         $bean->name = '<a href="'. $url .'">' . $bean->name . '</a>';
     }

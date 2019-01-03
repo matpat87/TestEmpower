b8767dfab3f4557fc77a18a664816027495a6f90
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2019-01-03 23:37:13 +0800

    Management Reports Module - Created new module "Management Reports which works similar to Sales Reports (Empower Reports) Module but will contain different reports with different access rights

---
+++ custom/modules/MGMT_ManagementReports/metadata/listviewdefs.php
@@ -0,0 +1,14 @@
+<?php
+$module_name = 'MGMT_ManagementReports';
+$listViewDefs [$module_name] = 
+array (
+  'NAME' => 
+  array (
+    'width' => '32%',
+    'label' => 'LBL_NAME',
+    'default' => true,
+    'link' => true,
+  ),
+);
+;
+?>

df499c3132ec3989a59eba867ced1bfb7979d44c
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2019-01-03 21:54:22 +0800

    OnTrack Module - Fixed issue where "Priority" filter is showing wrong values and displays incorrect data - Updated List View - Advanced Filter for Severity to display correct dropdown values and data

---
+++ custom/modules/OTR_OnTrack/metadata/searchdefs.php
@@ -0,0 +1,91 @@
+<?php
+$module_name = 'OTR_OnTrack';
+$_object_name = 'otr_ontrack';
+$searchdefs [$module_name] = 
+array (
+  'layout' => 
+  array (
+    'basic_search' => 
+    array (
+      0 => 'name',
+      1 => 
+      array (
+        'name' => 'current_user_only',
+        'label' => 'LBL_CURRENT_USER_FILTER',
+        'type' => 'bool',
+      ),
+      2 => 
+      array (
+        'name' => 'open_only',
+        'label' => 'LBL_OPEN_ITEMS',
+        'type' => 'bool',
+        'default' => false,
+        'width' => '10%',
+      ),
+    ),
+    'advanced_search' => 
+    array (
+      'otr_ontrack_number' => 
+      array (
+        'name' => 'otr_ontrack_number',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'name' => 
+      array (
+        'name' => 'name',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'resolution' => 
+      array (
+        'name' => 'resolution',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'status' => 
+      array (
+        'name' => 'status',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'severity_c' => 
+      array (
+        'type' => 'enum',
+        'default' => true,
+        'studio' => 'visible',
+        'label' => 'LBL_SEVERITY',
+        'width' => '10%',
+        'name' => 'severity_c',
+      ),
+      'assigned_user_id' => 
+      array (
+        'name' => 'assigned_user_id',
+        'type' => 'enum',
+        'label' => 'LBL_ASSIGNED_TO',
+        'function' => 
+        array (
+          'name' => 'get_user_array',
+          'params' => 
+          array (
+            0 => false,
+          ),
+        ),
+        'default' => true,
+        'width' => '10%',
+      ),
+    ),
+  ),
+  'templateMeta' => 
+  array (
+    'maxColumns' => '3',
+    'maxColumnsBasic' => '4',
+    'widths' => 
+    array (
+      'label' => '10',
+      'field' => '30',
+    ),
+  ),
+);
+;
+?>

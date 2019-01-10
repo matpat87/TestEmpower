441cec55c477ac11e3532ad6c81d9de302b7781e
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2019-01-10 22:46:23 +0800

    Accounts Module - Changed decimal point precision of Sales YTD field to 0

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_sls_ytd_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_sls_ytd_c.php
@@ -2,6 +2,6 @@
  // created: 2019-01-06 14:12:27
 $dictionary['Account']['fields']['sls_ytd_c']['inline_edit']='1';
 $dictionary['Account']['fields']['sls_ytd_c']['labelValue']='Sales YTD';
-$dictionary['Account']['fields']['sls_ytd_c']['precision']='2';
+$dictionary['Account']['fields']['sls_ytd_c']['precision']='0';
 
  ?>

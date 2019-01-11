c81bc81385cd4d61ebbeab3e41c64f00e3eec842
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2019-01-12 00:19:11 +0800

    Product Categories Module - Module already exists in Empower but is named as Products - Categories and was not displayed on the list - Added the following fields to match legacy empower: Product Category ID, ERP ID, Division, and Material Cost Type - Renamed from "Products - Categories" to "Product Categories" to match legacy empower module name - Added "Product Categories" in OnTrack Module field dropdown list - Updated Edit, Detail, and List views to match empower legacy

---
+++ custom/Extension/modules/AOS_Product_Categories/Ext/Vardefs/sugarfield_material_cost_type_c.php
@@ -0,0 +1,25 @@
+<?php
+	$dictionary['AOS_Product_Categories']['fields']['material_cost_type_c'] = array (
+		'inline_edit' => '1',
+		'labelValue' => 'Material Cost Type',
+		'required' => false,
+		'name' => 'material_cost_type_c',
+		'vname' => 'LBL_MATERIAL_COST_TYPE',
+		'type' => 'varchar',
+		'massupdate' => '0',
+		'default' => '',
+		'no_default' => false,
+		'comments' => '',
+		'help' => '',
+		'importable' => 'true',
+		'duplicate_merge' => 'disabled',
+		'duplicate_merge_dom_value' => '0',
+		'audited' => false,
+		'reportable' => true,
+		'unified_search' => false,
+		'merge_filter' => 'disabled',
+		'len' => '255',
+		'size' => '20',
+		'id' => 'AOS_Product_Categoriesmaterial_cost_type_c',
+	);
+?>
---
+++ custom/Extension/modules/AOS_Product_Categories/Ext/Vardefs/sugarfield_pcatid_c.php
@@ -0,0 +1,25 @@
+<?php
+	$dictionary['AOS_Product_Categories']['fields']['pcatid_c'] = array (
+		'inline_edit' => '1',
+		'labelValue' => 'Product Category ID',
+		'required' => true,
+		'name' => 'pcatid_c',
+		'vname' => 'LBL_PCATID',
+		'type' => 'varchar',
+		'massupdate' => '0',
+		'default' => '',
+		'no_default' => false,
+		'comments' => '',
+		'help' => '',
+		'importable' => 'true',
+		'duplicate_merge' => 'disabled',
+		'duplicate_merge_dom_value' => '0',
+		'audited' => false,
+		'reportable' => true,
+		'unified_search' => false,
+		'merge_filter' => 'disabled',
+		'len' => '255',
+		'size' => '20',
+		'id' => 'AOS_Product_Categoriespcatid_c',
+	);
+?>

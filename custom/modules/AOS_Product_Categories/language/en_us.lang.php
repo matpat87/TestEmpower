ff06de4f25f29ee51768953f54469a9528374f29
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2019-01-12 00:19:11 +0800

    Product Categories Module - Module already exists in Empower but is named as Products - Categories and was not displayed on the list - Added the following fields to match legacy empower: Product Category ID, ERP ID, Division, and Material Cost Type - Renamed from "Products - Categories" to "Product Categories" to match legacy empower module name - Added "Product Categories" in OnTrack Module field dropdown list - Updated Edit, Detail, and List views to match empower legacy

---
+++ custom/modules/AOS_Product_Categories/language/en_us.lang.php
@@ -0,0 +1,16 @@
+<?php
+// created: 2019-01-11 17:09:29
+$mod_strings = array (
+  'LBL_DIVISION' => 'Division',
+  'LBL_COMPANY_NO' => 'ERP ID',
+  'LBL_PCATID' => 'Product Category ID',
+  'LBL_MATERIAL_COST_TYPE' => 'Material Cost Type',
+  'LNK_NEW_RECORD' => 'Create Product Categories',
+  'LNK_LIST' => 'View Product Categories',
+  'LBL_LIST_FORM_TITLE' => 'Product Categories List',
+  'LBL_SEARCH_FORM_TITLE' => 'Search Product Categories',
+  'LBL_PRODUCT_CATEGORYS_NAME' => 'Parent category',
+  'LBL_SUB_CATEGORIES' => 'Sub categories',
+  'LBL_PARENT_CATEGORY' => 'Parent Category',
+  'LBL_HOMEPAGE_TITLE' => 'My Product Categories',
+);

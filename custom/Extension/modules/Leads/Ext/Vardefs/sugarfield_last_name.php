2cc47fc8de5bd95926a8fc2b5f53308c34ea9144
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2019-01-14 21:21:24 +0800

    Campaigns Module - Updated codes to remove required in Last Name field on Create Person Form - Leads

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_last_name.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2019-01-13 16:23:47
+$dictionary['Lead']['fields']['last_name']['required']=false;
+$dictionary['Lead']['fields']['last_name']['inline_edit']=true;
+$dictionary['Lead']['fields']['last_name']['comments']='Last name of the contact';
+$dictionary['Lead']['fields']['last_name']['merge_filter']='disabled';
+
+ ?>

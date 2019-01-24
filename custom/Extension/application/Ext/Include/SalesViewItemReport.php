ed80c7946b88cb382031468617e51900c311a5b9
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2019-01-24 21:27:23 +0800

    Sales View Item Report - Created new Sales View Item Report Module - Modified module to match Sales View Report behavior - Set report to fetch Sales View Item

---
+++ custom/Extension/application/Ext/Include/SalesViewItemReport.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['SVIR_SalesViewItemReport'] = 'SVIR_SalesViewItemReport';
+$beanFiles['SVIR_SalesViewItemReport'] = 'modules/SVIR_SalesViewItemReport/SVIR_SalesViewItemReport.php';
+$moduleList[] = 'SVIR_SalesViewItemReport';
+
+?>

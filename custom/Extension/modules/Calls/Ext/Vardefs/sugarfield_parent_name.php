66875fa3e428a2c604a27f0be31d67920e33f284
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2019-03-25 23:45:07 +0800

    Calls & Meetings Modules - Set "Related To" field to required and set default value to blank

---
+++ custom/Extension/modules/Calls/Ext/Vardefs/sugarfield_parent_name.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2019-03-25 15:19:54
+$dictionary['Call']['fields']['parent_name']['required']=true;
+
+ ?>
---
+++ custom/Extension/modules/Calls/Ext/Vardefs/sugarfield_parent_type.php
@@ -0,0 +1,9 @@
+<?php
+ // created: 2019-03-25 15:19:54
+$dictionary['Call']['fields']['parent_type']['required']=true;
+$dictionary['Call']['fields']['parent_type']['inline_edit']=true;
+$dictionary['Call']['fields']['parent_type']['options']='';
+$dictionary['Call']['fields']['parent_type']['comments']='The Sugar object to which the call is related';
+$dictionary['Call']['fields']['parent_type']['merge_filter']='disabled';
+
+ ?>

a86f357f4bf139b3f8f6415a9759b67181cbc4ff
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2019-03-27 23:31:10 +0800

    Global - Reverted files to previous state since these files should not be included in the commit

--- config.php
+++ config.php
@@ -96,8 +96,8 @@ $sugar_config = array (
   array (
     'db_host_name' => '127.0.0.1',
     'db_host_instance' => 'SQLEXPRESS',
-    'db_user_name' => 'root',
-    'db_password' => '',
+    'db_password' => 'abc123ABC!!',
+    'db_name' => 'suitecrm',
     'db_name' => 'suitecrm_qa',
     'db_type' => 'mysql',
     'db_port' => '',

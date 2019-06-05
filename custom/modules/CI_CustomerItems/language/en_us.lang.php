4ad678f8eff53dd701cbd4112763a0543c349cda
Ralph Julian B. Siasat <rsiasat00@gmail.com>
2019-06-05 13:43:39 +0800

    Customer Products Module - Renamed remaining labels that contained Customer Items to Customer Products

--- custom/modules/CI_CustomerItems/language/en_us.lang.php
+++ custom/modules/CI_CustomerItems/language/en_us.lang.php
@@ -1,5 +1,5 @@
 <?php
-// created: 2019-06-05 05:01:57
+// created: 2019-06-05 05:40:09
 $mod_strings = array (
   'LBL_NAME' => 'Product Name',
   'LBL_DATE_ENTERED' => 'Date Created',
@@ -9,4 +9,13 @@ $mod_strings = array (
   'LBL_CI_CUSTOMERITEMS_CI_CUSTOMERITEMS_FROM_CI_CUSTOMERITEMS_R_TITLE' => 'Customer Products',
   'LBL_MAINCODE' => 'Product Number (DD)',
   'LBL_PART_NUMBER' => 'Product Number',
+  'LBL_CI_CUSTOMERITEMS_SUBPANEL_TITLE' => 'Customer Products',
+  'LBL_LIST_FORM_TITLE' => 'Customer Products List',
+  'LBL_MODULE_NAME' => 'Customer Products',
+  'LBL_MODULE_TITLE' => 'Customer Products',
+  'LBL_NEW_FORM_TITLE' => 'New Customer Products',
+  'LBL_SEARCH_FORM_TITLE' => 'Search Customer Products',
+  'LNK_IMPORT_CI_CUSTOMERITEMS' => 'Import Customer Products',
+  'LNK_LIST' => 'View Customer Products',
+  'LNK_NEW_RECORD' => 'Create Customer Products',
 );
